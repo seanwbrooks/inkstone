f12c662f4319323be5690adbffe794dff66858fc
Sean <swbrooks87@gmail.com>
2018-01-22 15:13:07 -0500

    Add CRUD functions for Posts and validation

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,19 +1,35 @@
 class PostController < ApplicationController
   def index
+    @post = Post.find({})
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to post_path
+    end
+    render 'new'
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to post_path
+    end
+    render 'edit'
   end
 
   def destroy
+    @post = Post.destroy
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,9 @@ class Post
   field :author, type: String
   field :created_at, type: String
   field :updated_at, type: String
+
+  validates_presence_of :title
+  validates_uniqueness_of :title
+  validates_presence_of :body
+  validates_presence_of :author
 end
